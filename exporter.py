a6cf838220469b4c4201e926f41ada305048259d
Aurelius <113419203+Aureliustics@users.noreply.github.com>
2026-07-02 21:39:41 -0700

    chore: add colorama for styling prints/alerts based on type

--- exporter.py
+++ exporter.py
@@ -1,4 +1,5 @@
 import os
+import colorama
 import tkinter as tk
 from tkinter import filedialog
 
@@ -6,11 +7,15 @@ SAVE_DIR = None
 API = "https://graph.microsoft.com/v1.0"
 TOKEN = None
 FAIL_LOG = []
+colorama.init(autoreset=True)
+WARNING = "\033[38;2;255;165;0m"
+ERROR = "\033[31m"
+SUCCESS = "\033[92m"
 
 def find_location():
     parent_dir = filedialog.askdirectory(title="Choose Where to Save Export Data")
     if not parent_dir:
-        print("No folder selected so defaulting to Documents")
+        print(f"{WARNING}No folder selected so defaulting to Documents")
         os.path.join(os.path.expanduser("~"), "Documents", "Exports")
 
     os.makedirs(os.path.join(parent_dir, "Teams Export"), exist_ok=True)
